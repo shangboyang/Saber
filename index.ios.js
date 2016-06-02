aa3705536aa272f77b14b4c2076662425575f48e
shangboyang <yang8701@gmail.com>
2016-06-02 23:27:50 +0800

    update

--- index.ios.js
+++ index.ios.js
@@ -4,17 +4,7 @@
  * @flow
  */
 'use strict';
-/*
-var React = require('react-native');
 
-var {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    Image
-} = React;
-*/
 import React, { Component } from 'react';
 import {
   AppRegistry,
@@ -36,20 +26,7 @@ class Saber extends Component {
         );
     }
 }
-/*
-var Saber = React.createClass({
-    render: function () {
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>欢迎来到React Native的世界!</Text>
-                <Image style={{width: 50, height: 50, resizeMode: Image.resizeMode.contain}}
-                    source={{uri: 'https://facebook.github.io/react-native/img/header_logo.png'}}>
-                </Image>
-            </View>
-        );
-    }
-});
-*/
+
 var styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -64,52 +41,3 @@ var styles = StyleSheet.create({
 });
 
 AppRegistry.registerComponent('Saber', () => Saber);
-/*
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-class Saber extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          欢迎来到React Native的世界!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('Saber', () => Saber);
-*/
